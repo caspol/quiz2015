a1b1f49a60bc90fcdf258c68b429937b11f53633
caspol <franciscano_01@hotmail.com>
2015-07-16 11:55:36 +0200

    Entrega modulo 6

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 var quizController= require('../controllers/quiz_controller');
 
+var authorController= require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quizzzzz' });
+  res.render('index', { title: 'Quiz' });
 });
+
+router.get('/author',authorController.author);
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
